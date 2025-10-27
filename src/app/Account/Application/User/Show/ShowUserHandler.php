2138028ff3268f34178e5e528d84cea7ca1467c7
Vladislav Malikov <a0xh@4ifi.ru>
2025-10-27 20:57:14 +0500

    Auto commit 2025-10-27 20:57:14 #2797

---
+++ src/app/Account/Application/User/Show/ShowUserHandler.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Account\Application\Profile\Show;
+
+use App\Shared\Application\Handler;
+use App\Account\Domain\Repository\UserRepositoryInterface;
+use App\Shared\Domain\Id\UserId;
+
+final class ShowUserHandler extends Handler
+{
+    /**
+     * Constructs a new ShowUserHandler instance.
+     *
+     * @param UserRepositoryInterface $repository
+     */
+    public function __construct(
+        private UserRepositoryInterface $repository
+    ) {}
+
+    /**
+     * Handles ShowUserQuery to retrieve a User by ID.
+     *
+     * @param ShowUserQuery $query
+     * @return User|null
+     */
+    public function handle(ShowUserQuery $query): ?User
+    {
+        return $this->repository->findById(
+            id: UserId::fromString(value: $query->userId)
+        );
+    }
+}
